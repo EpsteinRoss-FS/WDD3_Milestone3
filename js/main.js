e70caa670a3e3ad5a64014c1fff64f8adb46ed08
Ross Epstein <repstein@student.fullsail.edu>
2021-12-10 08:16:13 -0500

    added model, app, controller, and view

---
+++ js/main.js
@@ -0,0 +1,46 @@
+class App {
+	constructor() {
+	  console.log("Creating app...");
+	  this.controller = new Controller();
+	}
+  
+	static getInstance() {
+	  if (!App.Instance) {
+		App.Instance = new App();
+  
+		return App.Instance;
+	  } else {
+		throw "Error!  An app is already created!";
+	  }
+	}
+  }
+
+
+class Controller {
+    constructor() 
+    {
+        console.log("Controller initialized...");
+        (this.model = new Model()),
+        (this.view = new View())
+
+    }
+}
+
+class Model {
+	constructor() 
+    {
+	  let lists;
+	  console.log("Model initialized...");  
+    }
+}
+
+class View {
+    constructor()
+    {
+        console.log("View Initialized...");
+    }
+}
+
+(function() {
+    this.app = App.getInstance();
+})();
