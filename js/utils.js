01315daa40c0906caeabdada3e72651f6f133f3a
Ross Epstein <repstein@student.fullsail.edu>
2021-12-10 16:00:53 -0500

    properly renders the list view

--- js/utils.js
+++ js/utils.js
@@ -4,38 +4,32 @@ class Utils{
    
     static faireListHTML(list) {
         this.html = "";
-        // for (let i = 0; i < list.length; i++) {
-        //   let title = list[i].title;
-        //   let items = list[i].items;
     
-        //   this.html += `<section class='board__column'><h2>${title} </h2>
-        //           <button class='addButton' id="${
-        //             list[i].id - 1
-        //           }">Add</button><ul>`;
-        //   for (let a = 0; a < items.length; a++) {
-        //     this.html += `<li><h3>${items[a].title}</h3>
-        //               <h4><time dateTime='${items[a].dueDate}'> Due Date: ${items[a].dueDate}</time></h4>
-        //               <p><em>Task Description</em>: ${items[a].description}</p></li>`;
-        //   }
-        //   this.html += `</ul></section>`;
-        // }
 
-        let html = `<div class="row">`
+        let html = `<div class="row faire-list-row">`
        
         for (let i = 0; i < list.length; i++)
         {
-        html += `<div class="col-lg-3 col-sm-6">'+
-        '                <div class="card-box bg-blue">'+
-        '                    <div class="inner">'+
-        '                        <h3> 13436 </h3>'+
-        '                        <p> Student Strength </p>'+
-        '                    </div>'+
-        '                    <div class="icon">'+
-        '                        <i class="fa fa-graduation-cap" aria-hidden="true"></i>'+
-        '                    </div>'+
-        '                    <a href="#" class="card-box-footer">View More <i class="fa fa-arrow-circle-right"></i></a>'+
-        '                </div>'+
-        '            </div>`;
+        html += `<div class="col-lg-10 faire-list-box">    
+                        <div class="card-box bg-info">
+                            <div class="inner">
+                                <h3> ${list[i].Name} </h3>
+                                <p> 
+                                    <div class="row">
+                                        <div class="col-sm-2">
+                                            ${list[i].Location}
+                                        </div>
+                                        <div class="col-sm-2">
+                                            ${list[i].Dates}
+                                        </div>
+                                    </div> 
+                                </p>
+                            </div>
+                            
+                            
+                            <a href="#" class="card-box-footer">View More <i class="fa fa-arrow-circle-right"></i></a>
+                        </div>
+                   </div>`;
         }
       
   
