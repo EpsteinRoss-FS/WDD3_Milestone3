22aeff14af734932a5bcab888dca1be5cc0c92e0
Ross Epstein <repstein@student.fullsail.edu>
2021-12-11 09:45:07 -0500

    beautified index

--- js/data.js
+++ js/data.js
@@ -1,7 +1,5 @@
 let api = "https://api.npoint.io/9d9a33985bc8d000eb50";
 
-// let key = "$2b$10$BNk2NfD6YHjUH0E9g5V67eSJDZso/Ow0QUupLrd1yC1.a3bmzleb2";
-
 class Data {
     static async getLists() {
         let listItems;
